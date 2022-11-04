72bb671981b1857e17273de4727bc33299c01240
Brayan Betancourt Ramirez <67015417+Betancourt1996@users.noreply.github.com>
2022-11-04 16:32:06 -0500

    Hasta el taller test empirico

---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/main.java
@@ -0,0 +1,17 @@
+package ec.edu.espol.workshops;
+import ec.edu.espol.workshops.CarInsurance;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CarInsurance nuevo = new CarInsurance();
+		
+		
+		nuevo.calculoPrima(99, "h", "c");
+		nuevo.calculoPrima(31, "h", "s");
+		nuevo.calculoPrima(18, "m", "s");
+		
+	}
+
+}
